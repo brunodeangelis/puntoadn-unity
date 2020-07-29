262d7c55a17fa095a845abcd221e657846da0518
Bruno De Angelis <brunordeangelis@gmail.com>
2020-07-29 16:54:07 -0300

    feat: base station spawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private List<GameObject> spawns;
+    private List<GameObject> stations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawns = GameObject.FindGameObjectsWithTag("StationSpawner").ToList();
+        stations = GameObject.FindGameObjectsWithTag("Station").ToList();
+        SpawnStations();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnStations()
+    {
+        Debug.Log(stations.Count);
+
+        for (int i = 0; i < spawns.Count; i++)
+        {
+            for (int j = stations.Count - 1; j >= 0; j--)
+            {
+                stations[j].transform.position = spawns[i].transform.position;
+                stations.RemoveAt(j);
+                break;
+            }
+        }
+
+        Debug.Log(stations.Count);
+    }
+}
--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -6,13 +6,7 @@ public class LookAt : MonoBehaviour
 {
     public Transform target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.DrawRay(transform.position, transform.forward, Color.green, 1000);
-        //Vector3 dir = target.position - transform.position;
-        //Vector3 lookRotation = Quaternion.LookRotation(dir, Vector3.up).eulerAngles;
-        //transform.rotation = Quaternion.Euler(0f, lookRotation.y, 0f);
+    void Update() {
         transform.LookAt(target, Vector3.up);
     }
 }
