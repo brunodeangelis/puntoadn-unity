5ec7dcc55a02bda8939e4abb731581156b053547
Bruno De Angelis <brunordeangelis@gmail.com>
2020-07-29 20:13:18 -0300

    feat: base station items spawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,35 +7,43 @@ public class GameManager : MonoBehaviour
 {
     private List<GameObject> spawns;
     private List<GameObject> stations;
+    private List<GameObject> stationItems;
 
-    // Start is called before the first frame update
     void Start()
     {
         spawns = GameObject.FindGameObjectsWithTag("StationSpawner").ToList();
         stations = GameObject.FindGameObjectsWithTag("Station").ToList();
-        SpawnStations();
-    }
+        stationItems = GameObject.FindGameObjectsWithTag("StationItem").ToList();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SpawnStations();
     }
 
     void SpawnStations()
     {
-        Debug.Log(stations.Count);
-
         for (int i = 0; i < spawns.Count; i++)
         {
             for (int j = stations.Count - 1; j >= 0; j--)
             {
+                FillStation(stations[j]);
                 stations[j].transform.position = spawns[i].transform.position;
                 stations.RemoveAt(j);
                 break;
             }
         }
+    }
 
-        Debug.Log(stations.Count);
+    void FillStation(GameObject station)
+    {
+        foreach (Transform child in station.transform)
+        {
+            if (child.CompareTag("StationItemSpawner"))
+            {
+                int randomIdx = Random.Range(0, stationItems.Count);
+                GameObject itemInstance = Instantiate(stationItems[randomIdx]);
+                itemInstance.transform.position = child.transform.position;
+                itemInstance.transform.SetParent(child.parent);
+                //stationItems.RemoveAt(randomIdx);
+            }
+        }
     }
 }
