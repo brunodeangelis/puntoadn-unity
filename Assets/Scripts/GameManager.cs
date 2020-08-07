1e546993a077d990f77b7dfd6a7844dd7ad2ea8b
Bruno De Angelis <brunordeangelis@gmail.com>
2020-08-07 18:15:18 -0300

    feat: paths & random colouring

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour
     private List<GameObject> _stations;
     private List<GameObject> _stationItems;
 
+    private GameObject[] _paths;
+    private MaterialPropertyBlock _materialPropertyBlock;
+
     public static Transform RecursiveFindChild(Transform parent, string childName)
     {
         Transform child = null;
@@ -32,16 +35,49 @@ public class GameManager : MonoBehaviour
         return child;
     }
 
-    void Start()
+    private int[] _hueValues = new int[] { 0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360 };
+    private List<int> _hueValuesList = new List<int>();
+
+    private void Start()
     {
         _spawns = GameObject.FindGameObjectsWithTag("StationSpawner").ToList();
         _stations = GameObject.FindGameObjectsWithTag("Station").ToList();
         _stationItems = GameObject.FindGameObjectsWithTag("StationItem").ToList();
 
         SpawnStations();
+
+        _paths = GameObject.FindGameObjectsWithTag("Path");
+
+        _hueValuesList = _hueValues.ToList();
     }
 
-    void SpawnStations()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int index = 0;
+            foreach (GameObject path in _paths)
+            {
+                path.GetComponent<Animator>().Play("path_scale");
+                index++;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            foreach (GameObject path in _paths)
+            {
+                MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+                
+                //Color color = Color.HSVToRGB(30f * (index + 1) / 360, 1, 1, true);
+                materialPropertyBlock.SetColor("_EmissionColor", Random.ColorHSV(0, 1, 1, 1, 1, 1) * 3);
+
+                path.transform.GetChild(0).GetComponent<Renderer>().SetPropertyBlock(materialPropertyBlock);
+                path.transform.GetChild(1).GetComponent<Renderer>().SetPropertyBlock(materialPropertyBlock);
+            }
+        }
+    }
+
+    private void SpawnStations()
     {
         for (int i = 0; i < _spawns.Count; i++)
         {
@@ -58,7 +94,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    void FillStation(GameObject station)
+    private void FillStation(GameObject station)
     {
         foreach (Transform child in station.transform)
         {
