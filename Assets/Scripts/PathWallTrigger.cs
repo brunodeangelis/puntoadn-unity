506c472bc6c45b64234fae97adf3c3eb9cc5d178
Bruno De Angelis <brunordeangelis@gmail.com>
2020-09-22 16:22:57 -0300

    feat: path walls

---
+++ Assets/Scripts/PathWallTrigger.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathWallTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject wall;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            wall.transform.DOScaleX(0.15f, 0.2f);
+            Destroy(gameObject);
+        }
+    }
+}
