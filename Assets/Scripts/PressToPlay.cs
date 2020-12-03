50c84b29b7794703e50871d16c2710ae687bb60d
Bruno De Angelis <brunordeangelis@gmail.com>
2020-12-03 15:09:12 -0300

    fix: "Can't play movie", add Label to some objects

--- Assets/Scripts/PressToPlay.cs
+++ Assets/Scripts/PressToPlay.cs
@@ -3,55 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
-using YoutubePlayer;
 
-namespace YoutubePlayer {
+[RequireComponent(typeof(VideoPlayer))]
+public class PressToPlay : MonoBehaviour {
+    private GameObject _ui;
 
-    [RequireComponent(typeof(VideoPlayer))]
-    public class PressToPlay : MonoBehaviour {
-        private GameObject _ui;
+    private bool _isPlayerLooking = false;
 
-        private bool _isPlayerLooking = false;
+    public GameObject _mainPlayer;
 
-        public GameObject _mainYoutubePlayer;
+    public static event Action<GameObject> OnMainVideoStartLoading;
 
-        public static event Action<GameObject> OnMainVideoStartLoading;
+    private void Awake() {
+        CloseVideo.OnCloseVideo += CloseVideo_OnCloseVideo;
 
-        private void Awake() {
-            CloseVideo.OnCloseVideo += CloseVideo_OnCloseVideo;
+        _ui = GameManager._i._interactText;
+    }
 
-            _ui = GameManager._i._interactText;
+    private void OnRaycastEnter(GameObject sender) {
+        if (sender.transform.tag == "Player") _isPlayerLooking = true;
+    }
+
+    private void OnRaycastExit(GameObject sender) {
+        if (sender.transform.tag == "Player") _isPlayerLooking = false;
+    }
+
+    private void Update() {
+        if (_isPlayerLooking && !GameManager._i._isVideoPlaying) {
+            //_ui.SetActive(true);
+
+            if (Input.GetMouseButtonDown(0))
+                PlayVideo();
+        } else {
+            //_ui.SetActive(false);
         }
+    }
 
-        private void OnRaycastEnter(GameObject sender) {
-            if (sender.transform.tag == "Player") _isPlayerLooking = true;
-        }
+    private void PlayVideo() {
+        _mainPlayer.GetComponent<VideoPlayer>().clip = GetComponent<VideoPlayer>().clip;
+        _mainPlayer.GetComponent<VideoPlayer>().enabled = true;
+        OnMainVideoStartLoading?.Invoke(_mainPlayer);
+    }
 
-        private void OnRaycastExit(GameObject sender) {
-            if (sender.transform.tag == "Player") _isPlayerLooking = false;
-        }
-
-        private void Update() {
-            if (_isPlayerLooking && !GameManager._i._isVideoPlaying)
-            {
-                //_ui.SetActive(true);
-
-                if (Input.GetMouseButtonDown(0))
-                    PlayVideo();
-            } else
-            {
-                //_ui.SetActive(false);
-            }
-        }
-        
-        private void PlayVideo() {
-            _mainYoutubePlayer.GetComponent<VideoPlayer>().clip = GetComponent<VideoPlayer>().clip;
-            _mainYoutubePlayer.GetComponent<VideoPlayer>().enabled = true;
-            OnMainVideoStartLoading?.Invoke(_mainYoutubePlayer);
-        }
-
-        private void CloseVideo_OnCloseVideo() {
-            _mainYoutubePlayer.GetComponent<VideoPlayer>().enabled = false;
-        }
+    private void CloseVideo_OnCloseVideo() {
+        _mainPlayer.GetComponent<VideoPlayer>().enabled = false;
     }
 }
--- Assets/Scripts/StationSpawner.cs
+++ Assets/Scripts/StationSpawner.cs
@@ -2,92 +2,81 @@
 using UnityEngine;
 using UnityEngine.Video;
 
-namespace YoutubePlayer
-{
-    public class StationSpawner : MonoBehaviour
-    {
-        public PathDirection _pathDirection;
+public class StationSpawner : MonoBehaviour {
+    public PathDirection _pathDirection;
 
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Player"))
-            {
-                if (GameManager._i._lastStationSpawned != null)
-                    Destroy(GameManager._i._lastStationSpawned);
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            if (GameManager._i._lastStationSpawned != null)
+                Destroy(GameManager._i._lastStationSpawned);
 
-                SpawnRandomStation();
+            SpawnRandomStation();
+        }
+    }
+    private void SpawnRandomStation() {
+        List<GameObject> stations = GameManager._i._stations;
+        List<Station> stationBlueprints = GameManager._i._stationBlueprints;
+
+        int r = Random.Range(0, stations.Count - 1);
+        int r2 = Random.Range(0, stationBlueprints.Count - 1);
+
+        Station stationBlueprint = Instantiate(stationBlueprints[r2]);
+        GameObject instancedStation = Instantiate(stations[r]);
+
+        FillStation(instancedStation, stationBlueprint);
+
+        // Ubico estación en la posición del "Station Position"
+        instancedStation.transform.position = transform.GetChild(0).transform.position;
+        instancedStation.transform.LookAt(Player.Instance.transform);
+
+        stations.RemoveAt(r);
+        stationBlueprints.RemoveAt(r2);
+
+        GameManager._i._stations = stations;
+        GameManager._i._stationBlueprints = stationBlueprints;
+        GameManager._i._lastStationSpawned = instancedStation;
+
+        Debug.Log("Station spawned.");
+
+        Destroy(gameObject);
+    }
+
+    private void FillStation(GameObject station, Station stationBlueprint) {
+        // Preparo para agarrar un stationItem aleatorio
+        int r = Random.Range(0, GameManager._i._stationItems.Count - 1);
+
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+
+        foreach (Transform child in station.transform) {
+            if (child.CompareTag("StationItemSpawner") && stationBlueprint._stationData._projects.Count > 0) {
+                // Agarro proyecto random de la lista
+                int r2 = Random.Range(0, stationBlueprint._stationData._projects.Count - 1);
+
+                Project project = stationBlueprint._stationData._projects[r2];
+
+                //Debug.Log(stationBlueprint._stationData._projects);
+                //Debug.Log(r2);
+
+                GameObject stationItem = Instantiate(GameManager._i._stationItems[r], child.parent);
+                stationItem.transform.position = child.transform.position;
+
+                Transform screen = stationItem.transform.Find("Screen");
+
+                screen.GetComponent<Label>()._text = $"Reproducir '{project._name}'";
+                screen.GetComponent<VideoPlayer>().clip = project._videoClip;
+                screen.GetComponent<VideoPlayer>().enabled = true;
+
+                // Saco el proyecto de la lista, ya lo usé
+                stationBlueprint._stationData._projects.Remove(project);
             }
         }
-        private void SpawnRandomStation()
-        {
-            List<GameObject> stations = GameManager._i._stations;
-            List<Station> stationBlueprints = GameManager._i._stationBlueprints;
 
-            int r = Random.Range(0, stations.Count - 1);
-            int r2 = Random.Range(0, stationBlueprints.Count - 1);
-
-            Station stationBlueprint = Instantiate(stationBlueprints[r2]);
-            GameObject instancedStation = Instantiate(stations[r]);
-
-            FillStation(instancedStation, stationBlueprint);
-
-            // Ubico estación en la posición del "Station Position"
-            instancedStation.transform.position = transform.GetChild(0).transform.position;
-            instancedStation.transform.LookAt(Player.Instance.transform);
-
-            stations.RemoveAt(r);
-            stationBlueprints.RemoveAt(r2);
-
-            GameManager._i._stations = stations;
-            GameManager._i._stationBlueprints = stationBlueprints;
-            GameManager._i._lastStationSpawned = instancedStation;
-
-            Debug.Log("Station spawned.");
-
-            Destroy(gameObject);
-        }
-
-        private void FillStation(GameObject station, Station stationBlueprint)
-        {
-            // Preparo para agarrar un stationItem aleatorio
-            int r = Random.Range(0, GameManager._i._stationItems.Count - 1);
-
-            MaterialPropertyBlock mpb = new MaterialPropertyBlock();
-
-            foreach (Transform child in station.transform)
-            {
-                if (child.CompareTag("StationItemSpawner") && stationBlueprint._stationData._projects.Count > 0)
-                {
-                    // Agarro proyecto random de la lista
-                    int r2 = Random.Range(0, stationBlueprint._stationData._projects.Count - 1);
-
-                    Project project = stationBlueprint._stationData._projects[r2];
-
-                    //Debug.Log(stationBlueprint._stationData._projects);
-                    //Debug.Log(r2);
-
-                    GameObject stationItem = Instantiate(GameManager._i._stationItems[r], child.parent);
-                    stationItem.transform.position = child.transform.position;
-
-                    Transform screen = stationItem.transform.Find("Screen");
-
-                    screen.GetComponent<Label>()._text = $"Reproducir '{project._name}'";
-                    screen.GetComponent<VideoPlayer>().clip = project._videoClip;
-                    screen.GetComponent<VideoPlayer>().enabled = true;
-
-                    // Saco el proyecto de la lista, ya lo usé
-                    stationBlueprint._stationData._projects.Remove(project);
-                }
-            }
-
-            // Saco stationItem de la lista, ya lo instancié
-            GameManager._i._stationItems.RemoveAt(r);
-        }
+        // Saco stationItem de la lista, ya lo instancié
+        GameManager._i._stationItems.RemoveAt(r);
     }
 }
 
-public enum PathDirection
-{
+public enum PathDirection {
     NORTH,
     EAST,
     SOUTH,
