6ece6a43ed9752d951deaeb1d6166becdd4ef2e2
Bruno De Angelis <brunordeangelis@gmail.com>
2020-12-16 01:55:10 -0300

    feat: play both brandbook videos

--- Assets/Scripts/PressToPlay.cs
+++ Assets/Scripts/PressToPlay.cs
@@ -7,13 +7,14 @@ using UnityEngine.Video;
 [RequireComponent(typeof(VideoPlayer))]
 public class PressToPlay : MonoBehaviour {
     [SerializeField] private bool _fadeBackgroundMusic = true;
+    [SerializeField] [Range(0f, 1f)] private float _volume = 1.0f;
     private AudioSource _musicFromVideo;
 
+    private VideoPlayer _videoPlayer;
     private GameObject _ui;
     private bool _isPlayerLooking = false;
 
     public GameObject _mainPlayer;
-    public VideoPlayer _videoPlayer;
     public static event Action<GameObject> OnMainVideoStartLoading;
     [HideInInspector] public bool _alreadySeen = false;
 
@@ -47,6 +48,7 @@ public class PressToPlay : MonoBehaviour {
         _videoPlayer.clip = GetComponent<VideoPlayer>().clip;
         _videoPlayer.SetTargetAudioSource(0, _musicFromVideo);
         _videoPlayer.enabled = true;
+        _musicFromVideo.volume = _volume;
 
         if (!_alreadySeen) GameManager._i._videosPlayed++;
         _alreadySeen = true;
